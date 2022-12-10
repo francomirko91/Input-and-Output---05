89595146eb4253cd4e01b59d1447987760d4646e
Mirko Franco <francomirko91@gmail.com>
2022-12-10 13:06:50 +0100

    ---------------------

---
+++ src/PrintYourAgeFromInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PrintYourAgeFromInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your age");
+        int age = input.nextInt();
+        System.out.printf("Your age is %d",age);
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
